2506304d3e10503b165b559958681c3b0cf57273
s-almeda <almedas1@tcnj.edu>
2019-10-16 02:03:47 -0400

    php fixfinal

--- form.php
+++ form.php
@@ -4,17 +4,17 @@
 $filename = "mydata.txt";
 $content = file($filename);
 
-$data = $_POST['field1'] . "\n" . "\r\n";
+$data = $_POST['field1'] . "\r\n";
 
-//insert to txt file
-$fp = fopen($filename,"a+");
+//insert votes to txt file
+$fp = fopen($filename,"a");
 fputs($fp,$data);
 fclose($fp);
 ?>
 
 
 
-<h2>Cool! Here's everyone's favorite memes so far:</h2>
+<h2>Result:</h2>
 
 <?php
 echo file_get_contents( "mydata.txt" ); // get the contents, and echo it out.
