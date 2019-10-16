3e46f3441a10d7c8f4d1d726c644b649f1642601
s-almeda <almedas1@tcnj.edu>
2019-10-16 00:03:35 -0400

    onloadtest

--- form.php
+++ form.php
@@ -1,44 +1,22 @@
 <?php
-$vote = $_REQUEST['vote'];
 
 //get content of textfile
-$filename = "poll_result.txt";
+$filename = "mydata.txt";
 $content = file($filename);
 
-//put content in array
-$array = explode("||", $content[0]);
-$yes = $array[0];
-$no = $array[1];
-
-if ($vote == 0) {
-  $yes = $yes + 1;
-}
-if ($vote == 1) {
-  $no = $no + 1;
-}
+$data = $_POST['field1'] . '-' . $_POST['field2'] . "\r\n";
 
 //insert votes to txt file
 $insertvote = $yes."||".$no;
 $fp = fopen($filename,"w");
-fputs($fp,$insertvote);
+fputs($fp,$data);
 fclose($fp);
 ?>
 
+
+
 <h2>Result:</h2>
-<table>
-<tr>
-<td>Yes:</td>
-<td>
-<img src="poll.gif"
-width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
-height='20'>
-<?php echo(100*round($yes/($no+$yes),2)); ?>%
-</td>
-</tr>
-<tr>
-<td>No:</td>
-<td>
-<?php echo(100*round($no/($no+$yes),2)); ?>%
-</td>
-</tr>
-</table>
+
+<?php
+echo file_get_contents( "mydata.txt" ); // get the contents, and echo it out.
+?>
